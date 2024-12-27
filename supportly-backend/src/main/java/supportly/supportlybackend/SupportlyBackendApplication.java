930ce9764545a45377ca720321292d5f0fd958d2
awieczorek <awieczorek0000@gmail.com>
2024-12-28 00:03:22 +0100

    first commit

---
+++ supportly-backend/src/main/java/supportly/supportlybackend/SupportlyBackendApplication.java
@@ -0,0 +1,13 @@
+package supportly.supportlybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportlyBackendApplication.class, args);
+	}
+
+}
---
+++ supportly-backend/src/test/java/supportly/supportlybackend/SupportlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package supportly.supportlybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
