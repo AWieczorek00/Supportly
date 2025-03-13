d7af1789020b8300dbc65f2192979d2ed5096195
awieczorek <awieczorek0000@gmail.com>
2025-03-13 21:26:30 +0100

    Add agreement

--- supportly-frontend/src/app/app.routes.ts
+++ supportly-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from './login/login.component';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {AgreementListComponent} from './agreement/agreement-list/agreement-list.component';
+import {AgreementAddComponent} from './agreement/agreement-add/agreement-add.component';
 
 export const routes: Routes = [
   {
@@ -13,6 +14,7 @@ export const routes: Routes = [
       { path: 'dsada', component:  AppComponent },
       { path: 'login', component: LoginComponent },
       { path: 'agreement/list', component: AgreementListComponent },
+      { path: 'agreement/add', component: AgreementAddComponent },
       // { path: 'orders', loadChildren: () => OrderListPageModule },
       // { path: 'new-order', loadChildren: () => NewOrderPageModule },
       // { path: 'task', loadChildren: () => AddTaskPageModule },
