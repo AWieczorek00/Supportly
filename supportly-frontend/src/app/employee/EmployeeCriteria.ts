5b8fe1dec3cf7764cf3636ef0a21c2baca5f738a
awieczorek <awieczorek0000@gmail.com>
2025-07-15 20:35:28 +0200

    Employee list and Employee add

---
+++ supportly-frontend/src/app/employee/EmployeeCriteria.ts
@@ -0,0 +1,6 @@
+export interface EmployeeCriteria{
+  firstName?: string | null;  // Możliwe wartości: string, null lub undefined
+  lastName?: string | null;  // Możliwe wartości: string lub null
+  phoneNumber?: string | null;  // Możliwe wartości: string lub null
+  role?: string | null;  // Możliwe wartości: string lub null
+}
