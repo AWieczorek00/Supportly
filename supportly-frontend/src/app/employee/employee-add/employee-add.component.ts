2894374f4ce929900bc6e97b65b4d01c98ac8dfc
Adam <92727986+AWieczorek00@users.noreply.github.com>
2025-07-16 21:16:12 +0200

    Employee list and Employee add (#79)
    * Employee list and Employee add
    
    * Hot-fix jenkinsfile
    
    * Hot-fix jenkinsfile

--- supportly-frontend/src/app/employee/employee-add/employee-add.component.ts
+++ supportly-frontend/src/app/employee/employee-add/employee-add.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialogActions, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
+import {MatOption} from '@angular/material/core';
+import {MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-employee-add',
-  imports: [],
+  imports: [
+    MatDialogContent,
+    MatFormField,
+    MatDialogActions,
+    MatDialogTitle,
+    MatInput,
+    ReactiveFormsModule,
+    MatButton,
+    MatLabel,
+    MatOption,
+    MatSelect
+  ],
   templateUrl: './employee-add.component.html',
-  styleUrl: './employee-add.component.scss'
+  styleUrl: './employee-add.component.scss',
 })
 export class EmployeeAddComponent {
 
+  constructor(
+    public dialogRef: MatDialogRef<EmployeeAddComponent>
+  ) {
+  }
+
+  create = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl(''),
+  });
+
+  submit(): void {
+    if (this.create.valid) {
+      this.dialogRef.close(this.create.value);
+    }
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
 }
