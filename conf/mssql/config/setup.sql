12b5914d435f5ad3f6bca53bfbaa10756bed905f
awieczorek <awieczorek0000@gmail.com>
2025-09-17 15:08:07 +0200

    healthcheck

--- conf/mssql/config/setup.sql
+++ conf/mssql/config/setup.sql
@@ -1,15 +1,21 @@
-CREATE DATABASE supportly;
+IF DB_ID('supportly') IS NULL
+    BEGIN
+        CREATE DATABASE supportly;
+    END
 GO
 
 USE supportly;
 GO
 
-CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'supportly')
+    BEGIN
+        CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
+        ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
+    END
 GO
 
--- Dodajemy linię, aby zapobiec wygasaniu hasła
-ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
-GO
-
-CREATE USER supportly FOR LOGIN supportly;
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'supportly')
+    BEGIN
+        CREATE USER supportly FOR LOGIN supportly;
+    END
 GO
