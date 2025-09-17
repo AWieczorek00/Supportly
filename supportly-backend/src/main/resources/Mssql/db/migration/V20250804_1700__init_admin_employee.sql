d5e964255d7b9e800b8c2b50e02a6f35980c7cdc
awieczorek <awieczorek0000@gmail.com>
2025-09-17 12:03:59 +0200

    Database alignment

---
+++ supportly-backend/src/main/resources/Mssql/db/migration/V20250804_1700__init_admin_employee.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (first_name, second_name, last_name, phone_number, email, data_of_creation)
+VALUES ('Admin', 'Head', 'SuperAdmin', '000000000', '', GETDATE());
+
+INSERT INTO employee (first_name, second_name, last_name, phone_number, email, data_of_creation)
+VALUES ('Admin', '', 'Admin', '000000001', '', GETDATE());
