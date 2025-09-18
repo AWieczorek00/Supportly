ff7aef1a7eef7f6332ca18d47698544a595ac0a9
awieczorek <awieczorek0000@gmail.com>
2025-09-18 12:27:00 +0200

    setup

--- conf/mssql/config/setup.sql
+++ conf/mssql/config/setup.sql
@@ -4,12 +4,21 @@ GO
 USE supportly;
 GO
 
-CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
+-- Tworzymy login, jeśli nie istnieje
+IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'supportly')
+    BEGIN
+        CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
+        ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
+    END
 GO
 
--- Dodajemy linię, aby zapobiec wygasaniu hasła
-ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
+-- Tworzymy użytkownika w bazie, jeśli nie istnieje
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'supportly')
+    BEGIN
+        CREATE USER supportly FOR LOGIN supportly;
+    END
 GO
 
-CREATE USER supportly FOR LOGIN supportly;
+-- Nadajemy pełne uprawnienia (db_owner) w bazie
+ALTER ROLE db_owner ADD MEMBER supportly;
 GO
