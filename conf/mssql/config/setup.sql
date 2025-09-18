8f1dea64d5ace80f068b311c6c85d4d10b80eded
awieczorek <awieczorek0000@gmail.com>
2025-09-18 12:12:42 +0200

    setup

--- conf/mssql/config/setup.sql
+++ conf/mssql/config/setup.sql
@@ -1,21 +1,15 @@
-IF DB_ID('supportly') IS NULL
-    BEGIN
-        CREATE DATABASE supportly;
-    END
+CREATE DATABASE supportly;
 GO
 
 USE supportly;
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'supportly')
-    BEGIN
-        CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
-        ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
-    END
+CREATE LOGIN supportly WITH PASSWORD = 'Qwerty.1';
 GO
 
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'supportly')
-    BEGIN
-        CREATE USER supportly FOR LOGIN supportly;
-    END
+-- Dodajemy linię, aby zapobiec wygasaniu hasła
+ALTER LOGIN supportly WITH PASSWORD = 'Qwerty.1', CHECK_POLICY = OFF;
+GO
+
+CREATE USER supportly FOR LOGIN supportly;
 GO
