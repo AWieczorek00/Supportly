9145cdb27cdf2a7af8af030fe56050a1fda1abd0
awieczorek <awieczorek0000@gmail.com>
2025-09-23 22:45:57 +0200

    init

--- conf/oracle/init-script/init_supportly.sql
+++ conf/oracle/init-script/init_supportly.sql
@@ -1,32 +1,37 @@
-        WHENEVER SQLERROR EXIT SQL.SQLCODE;
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
 
--- Przełączenie do PDB (Pluggable Database)
-        ALTER SESSION SET CONTAINER = ORCLPDB1;
+-- Nie ma potrzeby CONNECT, bo uruchamiasz z sysdba
 
--- Tablespace dla aplikacji
-        CREATE TABLESPACE SUPPORTLY_TS
-    DATAFILE '/opt/oracle/oradata/ORCLCDB/ORCLPDB1/supportly.dbf'
+CREATE TABLESPACE SUPPORTLY_TS
+    DATAFILE '/opt/oracle/oradata/ORCLCDB/supportly.dbf'
     SIZE 100M
     AUTOEXTEND ON NEXT 50M MAXSIZE 8G
     LOGGING
     EXTENT MANAGEMENT LOCAL UNIFORM SIZE 10M;
 
--- Tworzenie użytkownika aplikacyjnego
-        CREATE USER supportly IDENTIFIED BY "Qwerty_1"
+CREATE USER supportly IDENTIFIED BY "Qwerty_1"
     DEFAULT TABLESPACE SUPPORTLY_TS
     TEMPORARY TABLESPACE TEMP
     PROFILE DEFAULT
     ACCOUNT UNLOCK;
 
--- Podstawowe uprawnienia
-        GRANT CONNECT, RESOURCE TO supportly;
-        GRANT UNLIMITED TABLESPACE TO supportly;
-        GRANT CREATE SESSION TO supportly;
-        GRANT CREATE VIEW TO supportly;
+GRANT CONNECT TO supportly;
+GRANT RESOURCE TO supportly;
+GRANT UNLIMITED TABLESPACE TO supportly;
+GRANT ALTER ANY TABLE TO supportly;
+GRANT DELETE ANY TABLE TO supportly;
+GRANT INSERT ANY TABLE TO supportly;
+GRANT UPDATE ANY TABLE TO supportly;
+GRANT CREATE ANY TABLE TO supportly;
+GRANT CREATE ANY INDEX TO supportly;
+GRANT SELECT ANY TABLE TO supportly;
+GRANT CREATE ANY SEQUENCE TO supportly;
+GRANT DROP ANY INDEX TO supportly;
+GRANT CREATE SESSION TO supportly;
+GRANT CREATE VIEW TO supportly;
 
--- Uprawnienia do odczytu metadanych (jeśli aplikacja tego wymaga)
-        GRANT SELECT ON SYS.ALL_TAB_COLS TO supportly;
-        GRANT SELECT ON SYS.ALL_CONS_COLUMNS TO supportly;
-        GRANT SELECT ON SYS.DBA_CONSTRAINTS TO supportly;
+GRANT SELECT ON SYS.ALL_TAB_COLS TO supportly;
+GRANT SELECT ON SYS.ALL_CONS_COLUMNS TO supportly;
+GRANT SELECT ON SYS.DBA_CONSTRAINTS TO supportly;
 
-                EXIT;
+EXIT;
