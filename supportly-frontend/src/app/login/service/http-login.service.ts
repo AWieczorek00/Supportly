518bcace53ea0dad1a1ac611532632c77b7ff9c4
awieczorek <awieczorek0000@gmail.com>
2025-09-26 19:46:30 +0200

    Wrong url backend

--- supportly-frontend/src/app/login/service/http-login.service.ts
+++ supportly-frontend/src/app/login/service/http-login.service.ts
@@ -2,18 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import {Login} from '../Login';
+import {ConfigService} from '../../config/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpLoginService {
 
-  constructor(private client: HttpClient) {
+  constructor(private client: HttpClient, private configService: ConfigService) {
   }
 
   login(userDTO: Partial<Login>): Observable<{ token: string, expiresIn: number }> {
     console.log("leci rządanie")
-    return this.client.post<{ token: string, expiresIn: number }>('http://localhost:8080/auth/login', userDTO).pipe(map((res:any) => {
+    return this.client.post<{ token: string, expiresIn: number }>(`${this.configService.apiUrl}/auth/login`, userDTO).pipe(map((res:any) => {
       return res
     }));
   }
