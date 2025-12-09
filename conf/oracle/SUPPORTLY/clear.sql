88444c891230c1272f793225c873dc2fe7392034
awieczorek <awieczorek0000@gmail.com>
2025-12-09 19:58:49 +0100

    sql oracle

--- conf/oracle/SUPPORTLY/clear.sql
+++ conf/oracle/SUPPORTLY/clear.sql
@@ -1,26 +1,25 @@
--- 1. Ustawienie kontenera
-ALTER SESSION SET CONTAINER = ORCLCDB;
-ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
-
--- 2. Zabij sesje (w bloku PL/SQL, bezpiecznie)
 BEGIN
+    -- 1. ZABIJ SESJE
+    -- Pętla po wszystkich aktywnych sesjach użytkownika SUPPORTLY
     FOR s IN (SELECT sid, serial# FROM v$session WHERE username = 'SUPPORTLY') LOOP
-            EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || s.sid || ',' || s.serial# || ''' IMMEDIATE';
+            BEGIN
+                EXECUTE IMMEDIATE 'ALTER SYSTEM KILL SESSION ''' || s.sid || ',' || s.serial# || ''' IMMEDIATE';
+            EXCEPTION
+                WHEN OTHERS THEN
+                    NULL; -- Ignoruj błędy, jeśli sesja zdążyła się sama zamknąć
+            END;
         END LOOP;
-EXCEPTION
-    WHEN OTHERS THEN
-        NULL; -- Ignoruj błędy przy zabijaniu sesji
-END;
-/
 
--- 3. Usuń użytkownika (z obsługą błędu "nie istnieje")
-BEGIN
-    EXECUTE IMMEDIATE 'DROP USER supportly CASCADE';
-EXCEPTION
-    WHEN OTHERS THEN
-        -- Kod błędu ORA-01918 oznacza "user does not exist"
-        IF SQLCODE != -1918 THEN
-            RAISE; -- Jeśli to inny błąd, zgłoś go
-        END IF;
+    -- 2. USUŃ UŻYTKOWNIKA
+    BEGIN
+        EXECUTE IMMEDIATE 'DROP USER supportly CASCADE';
+    EXCEPTION
+        WHEN OTHERS THEN
+            -- Kod błędu -1918 oznacza "user does not exist"
+            IF SQLCODE != -1918 THEN
+                RAISE; -- Jeśli to inny błąd (np. brak uprawnień), zgłoś go i przerwij
+            END IF;
+        -- Jeśli błąd to -1918, nic nie rób (uznajemy za sukces)
+    END;
 END;
 /
