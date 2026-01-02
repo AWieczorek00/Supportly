c47f1d7737b7a1b28097a9ec9e859eeb8eb6e586
awieczorek <awieczorek0000@gmail.com>
2026-01-02 20:26:51 +0100

    change from edge to chrome

--- supportly-e2e/src/test/java/AgreementTest.java
+++ supportly-e2e/src/test/java/AgreementTest.java
@@ -18,7 +18,7 @@ public class AgreementTest extends TestDatabaseSetup {
 
     @BeforeEach
     void setup() throws Exception {
-        initDriver(false);
+        initDriver(true);
         loginAs("super.admin@gmail.com", "123456");
         Thread.sleep(500); // czekaj 0.5 sekundy
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
